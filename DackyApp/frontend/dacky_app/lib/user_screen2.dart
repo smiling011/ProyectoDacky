1b6e2540e15279273022b539c384eeaaba545798
smiling011 <victoriasaleck@gmail.com>
2025-09-26 20:38:27 -0500

    Cambio de IP para pruebas 2

--- DackyApp/frontend/dacky_app/lib/user_screen2.dart
+++ DackyApp/frontend/dacky_app/lib/user_screen2.dart
@@ -58,7 +58,7 @@ class _UserScreen2State extends State<UserScreen2> {
   Future<void> _guardarPerfil() async {
     if (idUsuario == null) return;
 
-    final url = Uri.parse("http://192.168.0.19:5000/perfil/$idUsuario");
+    final url = Uri.parse("http://10.1.117.38:5000/perfil/$idUsuario");
 
     final response = await http.put(
       url,
